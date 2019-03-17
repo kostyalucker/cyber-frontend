1d51372abafa33db0cd376b5d6c805ecbf7f1510
rob3n <roben1006@gmail.com>
2019-03-18 00:02:10 +0300

    [UPDATE] uncomment eventsource redux logic

--- background/src/index.js
+++ background/src/index.js
@@ -3,36 +3,35 @@ import { wrapStore } from 'webext-redux'
 import rootReducer from './reducers'
 
 const store = createStore(rootReducer, {})
+/*eslint-disable*/
+const eventSource = new EventSource('http://localhost:8080/sse')
 
-// const eventSource = new EventSource('http://localhost:8080/sse')
+eventSource.onopen = function(e) {
+  console.log(`Соединение открыто`)
+  console.log(e)
+}
 
-// // elsint-disable
-// eventSource.onopen = function(e) {
-//   console.log(`Соединение открыто`)
-//   console.log(e)
-// }
+eventSource.onerror = function(e) {
+  console.log(e)
+  eventSource.close()
+}
 
-// eventSource.onerror = function(e) {
-//   console.log(e)
-//   eventSource.close()
-// }
+eventSource.onmessage = function(e) {
+  // console.log(e.data);
+}
 
-// eventSource.onmessage = function(e) {
-//   // console.log(e.data);
-// }
-
-// eventSource.addEventListener('matches', function(e) {
-//   try {
-//     const jsonData = JSON.parse(e.data)
-//     console.log(jsonData.value)
-//     // store.dispatch({
-//     //   type: 'SET_MATCHES',
-//     //   payload: jsonData.value
-//     // })
-//     // console.log(store.getState())
-//   } catch (e) {
-//     console.log(`Ошибка ${e}`)
-//   }
-// })
+eventSource.addEventListener('matches', function(e) {
+  try {
+    const jsonData = JSON.parse(e.data)
+    console.log(jsonData.value)
+    store.dispatch({
+      type: 'SET_MATCHES',
+      payload: jsonData.value
+    })
+    // console.log(store.getState())
+  } catch (e) {
+    console.log(`Ошибка ${e}`)
+  }
+})
 
 wrapStore(store)
